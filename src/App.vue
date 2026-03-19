e16f1c1f940712fcc091056aa8e42bf0880096a2
jjabides <jj.abides@gmail.com>
2026-03-18 19:01:57 -0700

    install deck.gl and mapbox-gl

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { Deck } from '@deck.gl/core'
+import { ScatterplotLayer } from '@deck.gl/layers'
+
+const INITIAL_VIEW_STATE = {
+    latitude: 37.8,
+    longitude: -122.45,
+    zoom: 15,
+}
+
+const deckInstance = new Deck({
+    initialViewState: INITIAL_VIEW_STATE,
+    controller: true,
+    layers: [
+        new ScatterplotLayer({
+            data: [{ position: [-122.45, 37.8], color: [255, 0, 0], radius: 100 }],
+            getPosition: (d) => d.position,
+            getFillColor: (d) => d.color,
+            getRadius: (d) => d.radius,
+        }),
+    ],
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div></div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
-
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
-
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify styles
+import 'vuetify/styles' // Global Vuetify CSS
+import { createVuetify } from 'vuetify'
+
+// Vuetify components & directives
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Create vuetify instance
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
